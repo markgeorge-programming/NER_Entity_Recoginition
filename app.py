c10200e2dcb211df6eefefb2ced54085ee0a33fd
markgeorge-programming <markgeo2005@gmail.com>
2026-02-11 22:16:12 +0200

    Changed the model to be more accurate and added gazetteer to handle data unused in training

--- app.py
+++ app.py
@@ -6,12 +6,39 @@ import pickle
 
 app = Flask(__name__)
 
-# Constants (Must match training script)
+
 MAX_LENGTH = 20
 TAG_MAP = {0: "O", 1: "COLOR", 2: "PRODUCT", 3: "DATE", 4: "LOCATION"}
 
-# Load assets ONCE at startup
-model = tf.keras.models.load_model('ner (1).h5')
+product_db={"sneakers","shirts","shorts","boots","coat","hat","tshirt","shoes"}
+
+LOCATION_DB = {
+    "london", "paris", "new york", "dubai", "tokyo", "berlin",
+    "usa", "uk", "france", "germany", "japan",
+    "egypt", "cairo", "giza", "alexandria", "alex",
+    "helwan", "maadi", "zamalek", "nasr city", "6th of october",
+    "sharm", "hurghada", "dahab"
+}
+color_db = {
+    "red", "blue", "green", "yellow", "black", "white", "orange",
+    "purple", "pink", "brown", "grey", "gray", "silver", "gold"
+}
+date_db={"today", "yesterday", "tomorrow", "tonight",
+    "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday",
+    "january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december",
+    "weekend", "morning", "evening", "night"}
+
+"""
+ANIME_DB = {
+    "jujutsu", "kaisen", "gojo", "sukuna", "itadori",
+    "naruto", "sasuke", "sakura",
+    "bleach", "ichigo",
+    "luffy", "zoro", "one piece",
+    "blue lock", "isagi", "rin", "bachira"
+}
+"""
+model = tf.keras.models.load_model('ner_high_accuracy.h5')
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
@@ -22,21 +49,45 @@ def index():
     if request.method == 'POST':
         text = request.form.get('sentence', '')
 
-        # 1. Preprocess
+        # Preprocess
         seq = tokenizer.texts_to_sequences([text])
         padded = pad_sequences(seq, maxlen=MAX_LENGTH, padding='post')
 
-        # 2. Predict
+        # Predict
         pred = model.predict(padded)
         ids = np.argmax(pred, axis=-1)[0]
 
-        # 3. Format Output
+        # Format Output
         words = text.split()
         for i, word in enumerate(words):
             if i < MAX_LENGTH:
-                tag = TAG_MAP.get(ids[i], "O")
-                conf = np.max(pred[0][i])
-                results.append({"word": word, "tag": tag, "conf": f"{conf:.2%}"})
+                # Get the confidence score
+                raw_conf = np.max(pred[0][i])
+
+                #Gatekeeper Logic for low accuracies (hallucination error handling)
+                if raw_conf > 0.79:
+                    tag = TAG_MAP.get(ids[i], "O")
+                elif word.lower() in product_db:
+                    tag = "PRODUCT"
+                    raw_conf = 1.0
+                elif word.lower() in LOCATION_DB:
+                    tag = "LOCATION"
+                    raw_conf = 1.0
+                elif word.lower() in date_db:
+                    tag = "DATE"
+                    raw_conf = 1.0
+                elif word.lower() in color_db:
+                    tag="COLOR"
+                    raw_conf = 1.0
+                else:
+                    tag = "O"
+
+                # Append results
+                results.append({
+                    "word": word,
+                    "tag": tag,
+                    "conf": f"{raw_conf:.2%}"  # Show the raw score so you know
+                })
 
     return render_template('index.html', results=results)
 
